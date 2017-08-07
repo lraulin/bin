f687fc0836c52e70acbb4069f69573e3250d1cdd
Lee Raulin <leeraulin@gmail.com>
2017-08-07 11:44:59 -0400

    updated timer.py

--- timer.py
+++ timer.py
@@ -5,11 +5,15 @@ import os
 
 sec = 0
 minu = 0
+hour = 0
 cont = 0
 while cont != 1:
     print(minu, sec)
     time.sleep(1)
     if sec == 59:
+        if minu == 59:
+            minu = 0
+            hour += 1
         sec = 0
         minu = minu + 1
     else:
